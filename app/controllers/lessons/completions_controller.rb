d8094fb676c17490f19fd7f89d45190b560ca05f
Kevin <kevinmulhern23@gmail.com>
2023-03-03 07:33:26 +0000

    WIP

--- app/controllers/lessons/completions_controller.rb
+++ app/controllers/lessons/completions_controller.rb
@@ -1,36 +1,75 @@
 module Lessons
   class CompletionsController < ApplicationController
-    before_action :authenticate_user!
 
-    # rubocop: disable Metrics/MethodLength
     def create
-      lesson = Lesson.find(params[:lesson_id])
+      @lesson = Lesson.find(params[:lesson_id])
 
       lesson_completion = current_user.lesson_completions.new(
-        lesson:,
-        lesson_identifier_uuid: lesson.identifier_uuid,
-        course: lesson.course,
-        path: lesson.course.path,
+        lesson: @lesson,
+        lesson_identifier_uuid: @lesson.identifier_uuid,
+        course: @lesson.course,
+        path: @lesson.course.path,
       )
 
       if lesson_completion.save!
-        render json: lesson_completion, status: :created
+        respond_to do |format|
+          format.turbo_stream
+        end
       else
-        render json: { errors: lesson_completion.errors.full_messages }, status: :unprocessable_entity
+        redirect_to @lesson, notice: "Could not mark lesson complete"
       end
     end
-    # rubocop: enable Metrics/MethodLength
 
     def destroy
-      lesson = Lesson.find(params[:lesson_id])
-      lesson_completion = current_user.lesson_completions.find_by(lesson:)
+      @lesson = Lesson.find(params[:lesson_id])
+      lesson_completion = current_user.lesson_completions.find_by(lesson: @lesson)
 
       if lesson_completion.present?
         lesson_completion.destroy
-        render json: {}, status: :ok
+        respond_to do |format|
+          format.turbo_stream { render :create }
+        end
       else
-        render json: { errors: ['Lesson completion not found'] }, status: :not_found
+        redirect_to @lesson, notice: 'Could not mark lesson incomplete'
       end
     end
   end
 end
+
+
+# module Lessons
+#   class CompletionsController < ApplicationController
+#     before_action :authenticate_user!
+
+#     # rubocop: disable Metrics/MethodLength
+#     def create
+#       lesson = Lesson.find(params[:lesson_id])
+
+#       lesson_completion = current_user.lesson_completions.new(
+#         lesson:,
+#         lesson_identifier_uuid: lesson.identifier_uuid,
+#         course: lesson.course,
+#         path: lesson.course.path,
+#       )
+
+#       if lesson_completion.save!
+#         render json: lesson_completion, status: :created
+#       else
+#         render json: { errors: lesson_completion.errors.full_messages }, status: :unprocessable_entity
+#       end
+#     end
+#     # rubocop: enable Metrics/MethodLength
+
+#     def destroy
+#       lesson = Lesson.find(params[:lesson_id])
+#       lesson_completion = current_user.lesson_completions.find_by(lesson:)
+
+#       if lesson_completion.present?
+#         lesson_completion.destroy
+#         render json: {}, status: :ok
+#       else
+#         render json: { errors: ['Lesson completion not found'] }, status: :not_found
+#       end
+#     end
+#   end
+# end
